361824e6383bf01beebe09f112fdd7b0a9cfe74b
DoDoTeamSandA <dodosolutions4@gmail.com>
2021-06-13 09:09:55 +0530

    13/6/21 commit loginscreen

--- app/src/main/java/com/example/projectcovid19/Login.java
+++ app/src/main/java/com/example/projectcovid19/Login.java
@@ -1,25 +1,37 @@
 package com.example.projectcovid19;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TableLayout;
 
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.imageview.ShapeableImageView;
 import com.google.android.material.shape.CornerFamily;
+import com.google.android.material.tabs.TabLayout;
 
 public class Login extends AppCompatActivity {
+
+    private ExtendedFloatingActionButton floatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        /*ShapeableImageView imageView = findViewById(R.id.imageview);
+       /* ShapeableImageView imageView = findViewById(R.id.imageview);
           float radius = getResources().getDimension(R.dimen.default_corner_radius);
           imageView.setShapeAppearanceModel(imageView.getShapeAppearanceModel()
                .toBuilder()
-               .setBottomRightCorner(CornerFamily.ROUNDED,radius)
-               .setBottomLeftCorner(CornerFamily.ROUNDED,radius)
+
+                  .setAllCorners(CornerFamily.ROUNDED,radius)
                .build());*/
+        floatingActionButton = findViewById(R.id.login_button);
+
+
 
     }
 }
