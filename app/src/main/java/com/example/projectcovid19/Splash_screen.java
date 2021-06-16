3d71b8e9c181ec13a3944aedd993153dda3eecb5
DoDoTeamSandA <dodosolutions4@gmail.com>
2021-06-16 20:30:16 +0530

    16/6/21 commit Splash_screen

--- app/src/main/java/com/example/projectcovid19/Splash_screen.java
+++ app/src/main/java/com/example/projectcovid19/Splash_screen.java
@@ -2,13 +2,30 @@ package com.example.projectcovid19;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class Splash_screen extends AppCompatActivity {
-
+    private int time=4000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        ImageView img = (ImageView)findViewById(R.id.img);
+        Animation aniRotate = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotate_image);
+        img.startAnimation(aniRotate);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(Splash_screen.this,Login.class);
+                startActivity(intent);
+                finish();
+            }
+        },time);
     }
 }
