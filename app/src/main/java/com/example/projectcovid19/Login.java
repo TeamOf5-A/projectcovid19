fa59499b2e3abc0c00c691f979b1a8b138de5720
Ajay A <85162250+AjayBharadwaj-18@users.noreply.github.com>
2021-06-16 20:28:18 +0530

    otp

--- app/src/main/java/com/example/projectcovid19/Login.java
+++ app/src/main/java/com/example/projectcovid19/Login.java
@@ -1,22 +1,34 @@
 package com.example.projectcovid19;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.arch.core.executor.TaskExecutor;
 import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.format.Time;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TableLayout;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.imageview.ShapeableImageView;
 import com.google.android.material.shape.CornerFamily;
 import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
 
 public class Login extends AppCompatActivity {
 
     private ExtendedFloatingActionButton floatingActionButton;
+    private EditText etDigit1, etDigit2, etDigit3, etDigit4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,15 +41,47 @@ public class Login extends AppCompatActivity {
 
                   .setAllCorners(CornerFamily.ROUNDED,radius)
                .build());*/
-        floatingActionButton = findViewById(R.id.login_button);
-        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+        final EditText inputMobile = findViewById(R.id.mobileno);
+        Button buttonGetotp = findViewById(R.id.login_button);
+
+        buttonGetotp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Login.this,Otp_screen.class);
-                startActivity(intent);
+                if (inputMobile.getText().toString().trim().isEmpty()) {
+                    Toast.makeText(Login.this, "Enter Mobile", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                buttonGetotp.setVisibility(View.INVISIBLE);
+                PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                        "+91" + inputMobile.getText().toString(),
+                        60,
+                        TimeUnit.SECONDS,
+                        Login.this,
+                        new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                            @Override
+                            public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+                                buttonGetotp.setVisibility(View.VISIBLE);
+                            }
+
+                            @Override
+                            public void onVerificationFailed(@NonNull FirebaseException e) {
+                                buttonGetotp.setVisibility(View.VISIBLE);
+                                Toast.makeText(Login.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+
+                            @Override
+                            public void onCodeSent(@NonNull String login_buttonId, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                                super.onCodeSent(login_buttonId, forceResendingToken);
+                                buttonGetotp.setVisibility(View.VISIBLE);
+                                Intent intent = new Intent(getApplicationContext(), Otp_screen.class);
+                                intent.putExtra("mobileno", inputMobile.getText().toString());
+                                intent.putExtra("login_buttonId", login_buttonId);
+                                startActivity(intent);
+                            }
+                        }
+                );
+
             }
         });
-
-
     }
 }
--- app/src/main/java/com/example/projectcovid19/Otp_screen.java
+++ app/src/main/java/com/example/projectcovid19/Otp_screen.java
@@ -117,4 +117,5 @@ public class Otp_screen extends AppCompatActivity {
             }
         });
     }
+
 }
